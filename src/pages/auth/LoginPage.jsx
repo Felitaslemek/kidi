3375d40f642817c71d012ae47a7dbfeac10542b4
sbiimrly <imodaysbim07@gmail.com>
2025-02-02 21:29:40 +0700

    [+] init utils

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -55,7 +55,7 @@ const LoginPage = () => {
 	};
 
 	return (
-		<div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+		<div className="min-h-screen bg-gray-100">
 			<div className="bg-white w-full max-w-6xl rounded-lg shadow-lg overflow-hidden relative">
 				{/* Tombol Kembali */}
 				<button
