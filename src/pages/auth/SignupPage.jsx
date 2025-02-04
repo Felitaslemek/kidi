65a69ec6d0650d7352fc22e0db1ca4c7799a5c11
sbiimrly <imodaysbim07@gmail.com>
2025-02-04 13:02:31 +0700

    [~] fix design issues

--- src/pages/auth/SignupPage.jsx
+++ src/pages/auth/SignupPage.jsx
@@ -5,13 +5,13 @@ import {
 } from "../../firebase/firebaseConfig"; // Firebase Import
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
-import appPreview from "../../assets/appPreview.jpg";
 import {
 	getDocs,
 	query,
 	collection,
 	where,
 } from "firebase/firestore";
+import mockup from "../../assets/mockup.png";
 
 // Fungsi untuk mengecek apakah email sudah terdaftar
 const checkIfEmailExists = async (email) => {
@@ -89,156 +89,160 @@ const SignupPage = ({ onClose }) => {
 	};
 
 	return (
-		<div className="h-screen bg-gray-100 flex items-center justify-center p-4">
-			<div className="bg-white w-full max-w-6xl h-full lg:h-auto rounded-lg shadow-lg overflow-hidden relative">
-				{/* Tombol Kembali */}
-				<button
-					onClick={onClose}
-					className="absolute top-4 left-4 text-gray-600 hover:text-gray-800 text-lg font-semibold flex items-center space-x-2">
-					<span className="text-3xl">←</span>
-				</button>
-
+		<div className="bg-color_background_light p-6 lg:p-12">
+			<div className="">
 				{/* Notifikasi Berhasil */}
 				{isSuccess && (
-					<div className="absolute top-8 left-1/2 transform -translate-x-1/2 text-black text-lg font-semibold px-4 py-2">
+					<div className="">
 						Akun berhasil dibuat!
 					</div>
 				)}
+			</div>
 
-				<div className="flex flex-col lg:flex-row h-full">
-					{/* Form Signup */}
-					<div className="w-full lg:w-1/2 p-8 pt-16">
-						<div className="max-w-md mx-auto">
-							<h2 className="text-blue-500 text-center text-3xl font-semibold mb-2">
+			<div className="flex flex-col lg:flex-row">
+				{/* Tombol Kembali */}
+				<button
+					onClick={onClose}
+					className="absolute top-12 left-12">
+					<span className="">
+						<svg
+							width="24"
+							height="24"
+							viewBox="0 0 40 40"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg">
+							<path
+								d="M15.95 8.63338C16.2667 8.63338 16.5833 8.75005 16.8333 9.00005C17.3167 9.48338 17.3167 10.2834 16.8333 10.7667L7.6 20L16.8333 29.2334C17.3167 29.7167 17.3167 30.5167 16.8333 31C16.35 31.4834 15.55 31.4834 15.0667 31L4.95 20.8834C4.46666 20.4 4.46666 19.6 4.95 19.1167L15.0667 9.00005C15.3167 8.75005 15.6333 8.63338 15.95 8.63338Z"
+								fill="#717171"
+							/>
+							<path
+								d="M6.11669 18.75L34.1667 18.75C34.85 18.75 35.4167 19.3167 35.4167 20C35.4167 20.6833 34.85 21.25 34.1667 21.25L6.11669 21.25C5.43336 21.25 4.86669 20.6833 4.86669 20C4.86669 19.3167 5.43336 18.75 6.11669 18.75Z"
+								fill="#717171"
+							/>
+						</svg>
+					</span>
+				</button>
+				{/* Form Signup */}
+				<div className="w-full lg:w-1/2 flex flex-col align-items justify-center">
+					<div className="mx-auto">
+						<div className="flex flex-col gap-2">
+							<h2 className="text-color_primary_500_light text-center text-2xl font-semibold">
 								Buat Akun
 							</h2>
-							<p className="text-gray-400 text-center font-plusjakartasans mb-8 text-sm">
+							<p className="text-color_nuetral_300_light text-center text-sm">
 								Bergabung dan dapatkan informasi
-								baru dari kedai kami
-							</p>
-
-							<form
-								onSubmit={handleSubmit}
-								className="space-y-6">
-								<div>
-									<label
-										htmlFor="name"
-										className="block text-md font-semibold text-gray-700 mb-1">
-										Nama Anda
-									</label>
-									<input
-										type="text"
-										id="name"
-										value={name}
-										onChange={(e) =>
-											setName(e.target.value)
-										}
-										placeholder="Masukkan nama Anda"
-										required
-										className="w-full text-regular px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-									/>
-								</div>
-
-								<div>
-									<label
-										htmlFor="email"
-										className="block text-md font-semibold text-gray-700 mb-1">
-										Email Anda
-									</label>
-									<input
-										type="email"
-										id="email"
-										value={email}
-										onChange={(e) =>
-											setEmail(e.target.value)
-										}
-										placeholder="Masukkan email Anda"
-										required
-										className="w-full text-regular px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-									/>
-								</div>
-
-								<div>
-									<label
-										htmlFor="password"
-										className="block text-md font-semibold text-gray-700 mb-1">
-										Password
-									</label>
-									<div className="relative">
-										<input
-											type={
-												showPassword
-													? "text"
-													: "password"
-											}
-											id="password"
-											value={password}
-											onChange={(e) =>
-												setPassword(
-													e.target.value
-												)
-											}
-											placeholder="min. 8 karakter"
-											required
-											minLength={8}
-											className="w-full text-regular px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-										/>
-										<button
-											type="button"
-											onClick={() =>
-												setShowPassword(
-													!showPassword
-												)
-											}
-											className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500">
-											{showPassword ? "👁️" : "👁️‍🗨️"}
-										</button>
-									</div>
-								</div>
-
-								<button
-									type="submit"
-									className="w-full bg-blue-500 text-white font-semibold py-3 rounded-lg hover:bg-blue-600 transition-colors text-lg">
-									Buat Akun
-								</button>
-							</form>
-
-							<p className="mt-6 text-center font-semibold text-sm">
-								Sudah memiliki akun?{" "}
-								<a
-									href="/login"
-									className="text-blue-500 font-semibold hover:underline">
-									Masuk
-								</a>
-							</p>
-
-							<p className="mt-8 text-center text-xs text-gray-500">
-								© 2024 Kedai Manang. All rights
-								reserved.
+								baru <br />
+								dari kedai kami
 							</p>
 						</div>
-					</div>
 
-					{/* Gambar Samping */}
-					<div className="hidden lg:flex w-1/2 bg-blue-500 p-8 text-white items-center justify-center">
-						<div className="max-w-lg">
-							<h2 className="text-4xl font-semibold text-center mb-8">
-								Jajanan terjangkau
-								<br />
-								solusi perut lapar
-							</h2>
-							<div className="relative w-full max-w-md mx-auto h-auto rounded-3xl overflow-hidden shadow-xl">
-								<img
-									src={
-										appPreview ||
-										"/placeholder.svg"
+						<form
+							onSubmit={handleSubmit}
+							className="space-y-6">
+							<div>
+								<label
+									htmlFor="name"
+									className="block text-md font-semibold text-gray-700 mb-1">
+									Nama Anda
+								</label>
+								<input
+									type="text"
+									id="name"
+									value={name}
+									onChange={(e) =>
+										setName(e.target.value)
 									}
-									alt="App Preview"
-									className="w-full h-full object-cover"
+									placeholder="Masukkan nama Anda"
+									required
+									className="w-full text-regular px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
 								/>
 							</div>
-						</div>
+
+							<div>
+								<label
+									htmlFor="email"
+									className="block text-md font-semibold text-gray-700 mb-1">
+									Email Anda
+								</label>
+								<input
+									type="email"
+									id="email"
+									value={email}
+									onChange={(e) =>
+										setEmail(e.target.value)
+									}
+									placeholder="Masukkan email Anda"
+									required
+									className="w-full text-regular px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+								/>
+							</div>
+
+							<div>
+								<label
+									htmlFor="password"
+									className="block text-md font-semibold text-gray-700 mb-1">
+									Password
+								</label>
+								<div className="relative">
+									<input
+										type={
+											showPassword
+												? "text"
+												: "password"
+										}
+										id="password"
+										value={password}
+										onChange={(e) =>
+											setPassword(e.target.value)
+										}
+										placeholder="min. 8 karakter"
+										required
+										minLength={8}
+										className="w-full text-regular px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+									/>
+									<button
+										type="button"
+										onClick={() =>
+											setShowPassword(
+												!showPassword
+											)
+										}
+										className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500">
+										{showPassword ? "👁️" : "👁️‍🗨️"}
+									</button>
+								</div>
+							</div>
+
+							<button
+								type="submit"
+								className="w-full bg-blue-500 text-white font-semibold py-3 rounded-lg hover:bg-blue-600 transition-colors text-lg">
+								Buat Akun
+							</button>
+						</form>
+
+						<p className="mt-6 text-center font-semibold text-sm">
+							Sudah memiliki akun?{" "}
+							<a
+								href="/login"
+								className="text-blue-500 font-semibold hover:underline">
+								Masuk
+							</a>
+						</p>
+
+						<p className="mt-8 text-center text-xs text-gray-500">
+							© 2024 Kedai Manang. All rights
+							reserved.
+						</p>
 					</div>
+				</div>
+
+				{/* Gambar Samping */}
+				<div className="hidden lg:flex lg:h-1/2 w-1/2 items-center justify-center">
+					<img
+						src={mockup}
+						alt="App Preview"
+						className="w-full h-full object-cover"
+					/>
 				</div>
 			</div>
 		</div>
